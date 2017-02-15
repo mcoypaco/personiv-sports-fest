920daf525e5a50842f1f940656c15dae61417a9d
axassin <jiobuaron17@gmail.com>
2017-02-15 17:30:59 +0800

    fixing websocket

---
+++ socket.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Redis = require('ioredis');
+var redis = new Redis();
+
+redis.subscribe('changed.team', function(err, count){
+  console.log(count);
+  console.log(err);
+});
+
+redis.on('message', function(channel , message) {
+  console.log("message received");
+  console.log(channel);
+  message = JSON.parse(message);
+
+  io.emit(channel + ":" + message.event, message.payload);
+
+},function(err){
+  console.log("may error");
+});
+
+http.listen(3000, function(){
+  console.log("Server starting at port 3000");
+})
